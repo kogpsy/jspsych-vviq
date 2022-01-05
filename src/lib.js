16a8901c9140231872a533125c639064796e9d2a
Robin Bürkli <robuba.jr@gmx.ch>
2022-01-05 15:30:45 +0100

    Major refactor, simply export one generator function

---
+++ src/lib.js
@@ -0,0 +1,15 @@
+/*
+ * jspsych-vviq
+ *
+ * Author: Robin Bürkli <robuba.jr@gmx.ch>
+ * License: MIT
+ *
+ * This project aims to deliver a version of the VVIQ2 questionnaire which can
+ * be used in a jsPsych experiment.
+ *
+ * This file functions as an interface between the experiment and all the VVIQ
+ * items in different languages.
+ */
+
+// Re-export the main function
+export { generateVviqTimeline } from './generateVviqTimeline';
